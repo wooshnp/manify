9b6d596ad0627d1edebe6490cc3eab6994d5b6c6
wooshnp <nunojorgep@gmail.com>
2019-10-30 14:57:25 +0000

    added half of orders logic and UI

--- ManufactureApp/Sections/EditMenu/Cell/OrdersTableViewCell.swift
+++ ManufactureApp/Sections/EditMenu/Cell/OrdersTableViewCell.swift
@@ -8,17 +8,105 @@
 
 import UIKit
 
+enum OrderStatus {
+    case inProgress
+    case closed
+    
+    var color: UIColor
+    {
+        switch self {
+            case .closed:
+                return .green
+            case .inProgress:
+                return .yellow
+        }
+    }
+    
+}
+
+enum OrderType{
+    
+    case sales
+    case assignment
+    case tech
+    
+    var image: UIImage{
+        
+        switch self {
+            case .sales:
+                return #imageLiteral(resourceName: "sells")
+            case .assignment:
+                return #imageLiteral(resourceName: "order")
+            case .tech:
+                return #imageLiteral(resourceName: "technical")
+        }
+        
+    }
+    
+}
+
+struct Order {
+    let title: String
+    let description: String
+    let date: Date
+    let type: OrderType
+    let status: OrderStatus
+}
+
 class OrdersTableViewCell: UITableViewCell {
+    
+    static let identifier = "OrdersTableViewCell"
+    
+    @IBOutlet weak var orderImage: UIImageView!
+    @IBOutlet weak var orderTitle: UILabel!
+    @IBOutlet weak var orderDate: UILabel!
+    @IBOutlet weak var orderDescription: UILabel!
+    @IBOutlet weak var orderStatus: UIView!
+
+    
+    static func register(on tableView: UITableView)
+    {
+        let nib = UINib(nibName: identifier, bundle: .main)
+        tableView.register(nib, forCellReuseIdentifier: identifier)
+        tableView.rowHeight = 100
+        tableView.estimatedRowHeight = 100
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+                self.orderStatus.layer.cornerRadius = 40 / 2
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configure(with order: Order)
+    {
+        self.orderTitle.text = order.title
+        self.orderDescription.text = order.description
+        self.orderImage.image = order.type.image
+        self.orderImage.tintColor = .black
+        self.orderStatus.backgroundColor = order.status.color
+        
+        
+        let formatter = DateFormatter()
+        // initially set the format based on your datepicker date / server String
+        formatter.dateFormat = "dd/MM/yyyy"
+        let date = formatter.string(from: order.date)
+        self.orderDate.text = date
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.roundOrderStatus()
+    }
+    
+    
+    
+    func roundOrderStatus()
+    {
+        self.orderStatus.layer.borderWidth = 1
+        self.orderStatus.layer.borderColor = UIColor.gray.cgColor
+        self.orderStatus.layer.cornerRadius = 40 / 2
+        self.orderStatus.layer.masksToBounds = true
+
+    }
 }
--- ManufactureApp/Sections/EditMenu/EditMenuViewController.swift
+++ ManufactureApp/Sections/EditMenu/EditMenuViewController.swift
@@ -13,13 +13,17 @@ import Macaw
 class EditMenuViewController: UIViewController {
     
     
-  
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var fanMenu: FanMenu!
+    
+    var ordersList = [Order]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        fanMenu.backgroundColor = UIColor.init(named: "backgroundColor")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        OrdersTableViewCell.register(on: self.tableView)
+        fanMenu.backgroundColor = .clear
         
       fanMenu.button = FanMenuButton(
           id: "main",
@@ -48,14 +52,26 @@ class EditMenuViewController: UIViewController {
         
         
         
-        // call before animation
+        
         fanMenu.onItemDidClick = { button in
-          print("ItemDidClick: \(button.id)")
+          
+            switch button.id {
+                case "sells":
+                    let saleOrder = Order(title: "testesaleOrder", description: "teste", date: Date(), type: .sales, status: .inProgress)
+                    self.ordersList.append(saleOrder)
+                case "orders":
+                    let ordersOrder = Order(title: "testeordersOrder", description: "teste", date: Date(), type: .assignment, status: .inProgress)
+                    self.ordersList.append(ordersOrder)
+                case "tech":
+                    let techOrder = Order(title: "testetechOrder", description: "teste", date: Date(), type: .tech, status: .inProgress)
+                    self.ordersList.append(techOrder)
+                default:
+                    ()
+            }
+            
+            self.tableView.reloadData()
         }
-        // call after animation
-        fanMenu.onItemWillClick = { button in
-          print("ItemWillClick: \(button.id)")
-        }
+       
         
         // distance between button and items
         fanMenu.menuRadius = 75
@@ -75,4 +91,26 @@ class EditMenuViewController: UIViewController {
         
     }
 
+}
+
+extension EditMenuViewController: UITableViewDelegate
+{
+    
+}
+
+
+extension EditMenuViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.ordersList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: OrdersTableViewCell.identifier, for: indexPath) as! OrdersTableViewCell
+        cell.configure(with: self.ordersList[indexPath.row])
+        return cell
+    }
+    
+    
+    
 }
